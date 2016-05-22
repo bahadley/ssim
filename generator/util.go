bad5b72e2650144d6ba391771c29ec104e06a33c
Ben Hadley <bahadley@soe.ucsc.edu>
2016-05-21 17:05:33 -0700

    Added amplitude

--- generator/util.go
+++ generator/util.go
@@ -10,13 +10,17 @@ const (
 
 	// Frequency
 	hz = 261.625565
+
+	// Amplitude
+	amp = 128
 )
 
 func GenSinData(samples int) []byte {
 	f := (2.0 * math.Pi * hz) / rate
-	data := make([]byte, samples, samples)
-	for sample := 0; sample < samples; sample++ {
-		data[sample] = byte(64.0 * (math.Sin(f*float64(sample)) + 1.0))
+	ub := float64(amp / 2)
+	data := make([]byte, samples)
+	for i := 0; i < samples; i++ {
+		data[i] = byte(ub * (math.Sin(f*float64(i)) + 1.0))
 	}
 	return data
 }
