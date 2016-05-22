52a8efe63455608168b3ca8a5db6997882f93eaa
Ben Hadley <bahadley@soe.ucsc.edu>
2016-05-22 12:12:54 -0700

    Simplified GenSineData

--- generator/util.go
+++ generator/util.go
@@ -5,22 +5,18 @@ import (
 )
 
 const (
-	// Number of samples per second
-	rate = 5000
-
 	// Frequency
-	hz = 260
+	hz = .2
 
 	// Amplitude (peak-to-peak)
 	amp = 128
 )
 
 func GenSineData(tuples []*SensorTuple) error {
-	f := (2.0 * math.Pi * hz) / rate
-	ub := float64(amp / 2)
+	peakAmp := float64(amp / 2)
 
 	for i := 0; i < len(tuples); i++ {
-		tuples[i].Data = ub * (math.Sin(f*float64(i)) + 1.0)
+		tuples[i].Data = peakAmp * (math.Sin(hz*float64(i)) + 1.0)
 		tuples[i].Data = toFixed(tuples[i].Data, 2)
 	}
 
