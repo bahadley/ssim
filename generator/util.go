99e9bb963a2e609ee1e664500e1d0ea92061f528
Ben Hadley <bahadley@soe.ucsc.edu>
2016-05-21 17:06:56 -0700

    Formatting

--- generator/util.go
+++ generator/util.go
@@ -18,9 +18,12 @@ const (
 func GenSinData(samples int) []byte {
 	f := (2.0 * math.Pi * hz) / rate
 	ub := float64(amp / 2)
+
 	data := make([]byte, samples)
+
 	for i := 0; i < samples; i++ {
 		data[i] = byte(ub * (math.Sin(f*float64(i)) + 1.0))
 	}
+
 	return data
 }
